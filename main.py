7fcf6e3a2fd9903197021faced0ce1e8c81ed066
Alexandru Calugari <afishr00@gmail.com>
2020-10-11 17:14:02 +0300

    Make requests to all routes
    Still can't access the last ones in time: /route/3/3/1 & /route/3/3/2

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 import concurrent.futures
 
 def makeRequest(path, token=''):
+	print('GET ' + path)
 	BASE_URL = 'http://0.0.0.0:5000'
 	link = BASE_URL + path
 	return requests.get(link, headers={'X-Access-Token': token}).json()
@@ -10,8 +11,16 @@ accessToken = makeRequest('/register')['access_token']
 links = makeRequest('/home', accessToken)['link']
 
 with concurrent.futures.ThreadPoolExecutor() as executor:
-	results = [executor.submit(makeRequest, links[key], accessToken) for key in links]
+	queue = [executor.submit(makeRequest, links[key], accessToken) for key in links]
+	results = []
 
-	for f in concurrent.futures.as_completed(results):
-		print(f.result())
-		print('_____________')
+	while queue:
+		response = queue.pop(0).result()
+		results.append(response)
+
+		if 'link' in response and 'msg' not in response:
+			links = response['link']
+			for key in links:
+				queue.append(executor.submit(makeRequest, links[key], accessToken))
+
+print(results)
