391c0bd9d76c784419d6a372f481148af296e99c
Alexandru Calugari <afishr00@gmail.com>
2020-10-12 20:53:51 +0300

    Finish fetching part

--- main.py
+++ main.py
@@ -2,25 +2,28 @@ import requests
 import concurrent.futures
 
 def makeRequest(path, token=''):
-	print('GET ' + path)
+	# print('GET ' + path)
 	BASE_URL = 'http://0.0.0.0:5000'
 	link = BASE_URL + path
 	return requests.get(link, headers={'X-Access-Token': token}).json()
 
-accessToken = makeRequest('/register')['access_token']
-links = makeRequest('/home', accessToken)['link']
+def main():
+	accessToken = makeRequest('/register')['access_token']
+	links = makeRequest('/home', accessToken)['link']
 
-with concurrent.futures.ThreadPoolExecutor() as executor:
-	queue = [executor.submit(makeRequest, links[key], accessToken) for key in links]
-	results = []
+	with concurrent.futures.ThreadPoolExecutor() as executor:
+		queue = [executor.submit(makeRequest, links[key], accessToken) for key in sorted(links.keys(), reverse=True)]
+		results = []
 
-	while queue:
-		response = queue.pop(0).result()
-		results.append(response)
+		while queue:
+			response = queue.pop().result()
+			results.append(response)
 
-		if 'link' in response and 'msg' not in response:
-			links = response['link']
-			for key in links:
-				queue.append(executor.submit(makeRequest, links[key], accessToken))
+			if 'link' in response and 'msg' not in response:
+				links = response['link']
+				for key in links:
+					queue.append(executor.submit(makeRequest, links[key], accessToken))
+	
+	print(results)
 
-print(results)
+main()
