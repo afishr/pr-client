81134e261197c52e622803c20b3a7ffda555cc7b
Alexandru Calugari <afishr00@gmail.com>
2020-10-13 16:46:53 +0300

    Fix queue

--- fetcher.py
+++ fetcher.py
@@ -1,8 +1,8 @@
 import requests
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor, FIRST_COMPLETED, wait
 
 class Fetcher:
-  
+
   def __init__(self, baseUrl):
     self.BASE_URL = baseUrl
     self.token = ''
@@ -11,7 +11,7 @@ class Fetcher:
     print('GET', path)
     link = self.BASE_URL + path
     return requests.get(link, headers={'X-Access-Token': self.token}).json()
-  
+
   def __authenticate(self):
     self.token = self.__makeRequest('/register')['access_token']
 
@@ -19,17 +19,20 @@ class Fetcher:
     self.__authenticate()
     links = self.__makeRequest('/home')['link']
 
-    with ThreadPoolExecutor() as executor:
-      queue = [executor.submit(self.__makeRequest, links[key]) for key in links]
-      results = []
+    executor = ThreadPoolExecutor(max_workers=6)
+    queue = [executor.submit(self.__makeRequest, links[key]) for key in links]
+    results = []
 
-      while not queue.empty():
-        response = queue.pop(0).result()
-        results.append(response)
+    while queue:
+      done, queue = wait(queue, return_when=FIRST_COMPLETED)
 
-        if 'link' in response and 'msg' not in response:
-          links = response['link']
+      for el in done:
+        result = el.result()
+        results.append(result)
+
+        if 'link' in result and 'msg' not in result:
+          links = result['link']
           for key in links:
-            queue.append(executor.submit(self.__makeRequest, links[key]))
-    
+            queue.add(executor.submit(self.__makeRequest, links[key]))
+
     return results
