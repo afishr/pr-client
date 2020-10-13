d74a036e06ecd75cfeee6a0248b647f22b25f028
Alexandru Calugari <afishr00@gmail.com>
2020-10-13 15:58:06 +0300

    Convert tabs to spaces

--- fetcher.py
+++ fetcher.py
@@ -1,35 +1,35 @@
 import requests
 from concurrent.futures import ThreadPoolExecutor
 
-class Fetcher():
-	
-	def __init__(self, baseUrl):
-		self.BASE_URL = baseUrl
-		self.token = ''
+class Fetcher:
+  
+  def __init__(self, baseUrl):
+    self.BASE_URL = baseUrl
+    self.token = ''
 
-	def __makeRequest(self, path):
-		print('GET', path)
-		link = self.BASE_URL + path
-		return requests.get(link, headers={'X-Access-Token': self.token}).json()
-	
-	def __authenticate(self):
-		self.token = self.__makeRequest('/register')['access_token']
+  def __makeRequest(self, path):
+    print('GET', path)
+    link = self.BASE_URL + path
+    return requests.get(link, headers={'X-Access-Token': self.token}).json()
+  
+  def __authenticate(self):
+    self.token = self.__makeRequest('/register')['access_token']
 
-	def fetch(self):
-		self.__authenticate()
-		links = self.__makeRequest('/home')['link']
+  def fetch(self):
+    self.__authenticate()
+    links = self.__makeRequest('/home')['link']
 
-		with ThreadPoolExecutor() as executor:
-			queue = [executor.submit(self.__makeRequest, links[key]) for key in links]
-			results = []
+    with ThreadPoolExecutor() as executor:
+      queue = [executor.submit(self.__makeRequest, links[key]) for key in links]
+      results = []
 
-			while queue:
-				response = queue.pop(0).result()
-				results.append(response)
+      while not queue.empty():
+        response = queue.pop(0).result()
+        results.append(response)
 
-				if 'link' in response and 'msg' not in response:
-					links = response['link']
-					for key in links:
-						queue.append(executor.submit(self.__makeRequest, links[key]))
-		
-		return results
+        if 'link' in response and 'msg' not in response:
+          links = response['link']
+          for key in links:
+            queue.append(executor.submit(self.__makeRequest, links[key]))
+    
+    return results
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from fetcher import Fetcher
 
 def main():
-	results = Fetcher('http://localhost:5000').fetch()
+  results = Fetcher('http://localhost:5000').fetch()
 
-	for element in results:
-		print()
-		print(element)
-		print()
+  for element in results:
+    print()
+    print(element)
+    print()
 
 if __name__ == "__main__":
-	main()
+  main()
