bb37365420f0a1158e7cedc9feceddad12540021
afishr <afishr00@gmail.com>
2025-06-05 22:54:56 +0200

    feat: add caching per request

--- fetcher.py
+++ fetcher.py
@@ -1,21 +1,30 @@
 import requests
+from diskcache import Cache
 from concurrent.futures import ThreadPoolExecutor, FIRST_COMPLETED, wait
+
 
 class Fetcher:
 
   def __init__(self, baseUrl):
     self.BASE_URL = baseUrl
     self.token = ''
+    self.cache = Cache('./tmp/cache/')
 
   def __makeRequest(self, path):
     link = self.BASE_URL + path
-    response = requests.get(link, headers={'X-Access-Token': self.token})
+
+    if link in self.cache:
+      response = self.cache[link]
+    else:
+      response = requests.get(link, headers={'X-Access-Token': self.token})
 
     if response.headers.get('Content-Type') == 'application/json':
-        return response.json()
-    else:
-        raise ValueError("Expected JSON but got something else: " + response.text)
+      self.cache[link] = response
 
+      return response.json()
+    else:
+      raise ValueError(
+        "Expected JSON but got something else: " + response.text)
 
   def __authenticate(self):
     self.token = self.__makeRequest('/register')['access_token']
